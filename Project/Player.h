7bd871151b554e723532cdbab56bf4e8532f780a
heee123 <67463101+heee123@users.noreply.github.com>
2020-07-01 17:30:50 +0800

    Update Player.h

--- Project/Player.h
+++ Project/Player.h
@@ -4,21 +4,21 @@ class Player{
 public:
 	Player(Game &game);
 	void NewGame();         //开始一局
-    int CallLord();         //玩家叫地主，返回分数
-    void GetCards();    //发牌（抓一张牌，放入cards数组中）
+    //bool CallLord();         //玩家叫地主
+    void GetCards(int num);    //发牌（抓一张牌，放入cards数组中）
 	int GetRemain();        //获取玩家当前剩余牌数
-    void AddSelection();    //（根据玩家操作）增加一张选牌
-    void DeleteSelection(); //（根据玩家操作）取消一张选牌
-	bool IsRight();         //判断selection里的牌是否符合出牌规则
+    void AddSelection(int num);    //（根据玩家操作）增加一张选牌
+    void DeleteSelection(int num); //（根据玩家操作）取消一张选牌
+	bool IsRight(Type type);         //判断selection里的牌是否符合出牌规则
 	CardGroup Give();       //将合规的selection牌组打出，并且更新现有牌
-    void Pass(void);        //玩家选择不出，过牌
+    void Pass();        //玩家选择不出，过牌
 
+//对于AI玩家，首先判断是否过牌，若否，则选牌，之后出牌
 	void AISelect();       //AI选牌,包含一些简单的判读分析，比如直接出牌还是跟上家的牌
-	void AIPass(void);        //AI选择不出，过牌
+	bool AIPass(void);        //AI选择不出，过牌
 
 private:
 	Game &game;             //游戏对象
-	int score;              //玩家叫地主分数
-	int cards[35];          //手中所持牌的集合
+	std::set<int> cards;          //手中所持牌的集合
 	CardGroup selection;    //玩家选择，待出的牌的集合
 };
